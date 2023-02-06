91113a2e9a4d2b1553018c84cd947c4ab2c3521a
baqar-abbas <baqar007@hotmail.com>
2023-02-06 21:31:19 +0500

    Added linter.yml and JS linters Configuration done in Collaboration with my Coding partner Kelvin Irungu

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-//Popup JS
+// Popup JS
 const navMenu = document.querySelector('.nav-menu');
 const cross = document.querySelector('#cross');
 const mobileMenu = document.querySelector('.mobile-menu');
 const hamImage = document.querySelector('.imageone');
 
 const menuOpen = (event) => {
-    navMenu.style.display ='none';
-    mobileMenu.style.display = 'flex';
-    mobileMenu.style.width = '375px'; 
-    event.preventDefault();
-}
+  navMenu.style.display = 'none';
+  mobileMenu.style.display = 'flex';
+  mobileMenu.style.width = '375px';
+  event.preventDefault();
+};
 const menuClose = () => {
-    navMenu.style.display = 'block';
-    mobileMenu.style.display = 'none';
-}
+  navMenu.style.display = 'block';
+  mobileMenu.style.display = 'none';
+};
 
-hamImage.addEventListener('click',menuOpen);
-cross.addEventListener('click',menuClose);
-mobileMenu.addEventListener('click',menuClose);
+hamImage.addEventListener('click', menuOpen);
+cross.addEventListener('click', menuClose);
+mobileMenu.addEventListener('click', menuClose);
